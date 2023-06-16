dff05baafacbef2bf33b309b21dd0e9b59f467e0
zhangjinzhe <zhangjinzhe@88.com>
2023-06-16 17:01:16 +0800

    迁移文章

--- dosc/.vitepress/config.js
+++ dosc/.vitepress/config.js
@@ -4,18 +4,40 @@ import router from "./router";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  lang: 'zh-CN',
   outDir: "../dist",
-  title: "云深",
+  title: "云深的博客",
   description: "",
+  lastUpdated: true,
+  returnToTopLabel: true,
+  markdown: {
+    lineNumbers: true
+  },
   themeConfig: {
+    algolia: {
+      appId: 'PDEGHFPKIN',
+      apiKey: '6f186bf493b18009f5fd2b63a468d0e1',
+      indexName: 'yunshen'
+    },
     logo: {
       light: '/images/logo.png',
       dark: '/images/logo_.png',
     },
-    // https://vitepress.dev/reference/default-theme-config
+    footer: {
+      message: '鲁ICP备2022007356号-1',
+      copyright: 'Copyright © 2020 - present 云深</a>'
+    },
     nav: routers.nav,
     sidebar: router.sidebar,
-
+    docFooter: {
+      prev: '上一篇',
+      next: '下一篇'
+    },
+    outline: {
+      level: 'deep',
+      label: '本页目录'
+    },
+    lastUpdatedText: '上次更新',
     socialLinks: [
       { icon: 'github', link: 'https://github.com/yunshenpro' }
     ]
--- dosc/index.md
+++ dosc/index.md
@@ -3,7 +3,7 @@
 layout: home
 
 hero:
-  name: "云深的博客"
+  name: "云深"
   text: 
   tagline: 路漫漫其修远兮，我将上下而求索。
 #  actions:
@@ -14,15 +14,17 @@ hero:
 #      text: API Examples
 #      link: /api-examples
 
-#features:
-#  - icon: 👨
-#    title: 我的名字
-#    details: 松下问童子，言师采药去。只在此山中，*云深*不知处。
-#  - icon: 🗺
-#    title: 工作经历
-#    details: 我曾任职北京众安保险前端开发、山东青岛某科技公司前端leader，现任职山东青岛某国企前端开发。
-#  - icon: ⌛
-#    title: 座右铭
-#    details: 路漫漫其修远兮，吾将上下而求索。
+features:
+  - icon: 👨
+    title: 我的名字
+    details: 松下问童子，言师采药去。只在此山中，*云深*不知处。
+  - icon: 🗺
+    title: 工作经历
+    details: 我曾任职北京众安保险前端开发、山东青岛某科技公司前端leader，现任职山东青岛某国企前端开发。
+  - icon: ⌛
+    title: 座右铭
+    details: 路漫漫其修远兮，吾将上下而求索。
 ---
+
+
 
