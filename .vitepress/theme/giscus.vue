cffd5eb75ad558cfe4c3dfff123d2e812dd6f43f
zhangjinzhe <zhangjinzhepro@qq.com>
2023-09-14 15:36:50 +0800

    添加评论

--- .vitepress/theme/giscus.vue
+++ .vitepress/theme/giscus.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="comments">
     <giscus
-      data-repo="zhangjinzhepro/blog"
-      data-repo-id="MDEwOlJlcG9zaXRvcnkyNTI4OTA0OTM="
-      data-category="General"
-      data-category-id="DIC_kwDODxLNfc4CV3Ab"
-      data-mapping="pathname"
-      data-strict="0"
-      data-reactions-enabled="1"
-      data-emit-metadata="0"
-      data-input-position="top"
-      data-theme="preferred_color_scheme"
-      data-lang="zh-CN"
-      data-loading="lazy"
+      repo="zhangjinzhepro/blog"
+      repo-id="MDEwOlJlcG9zaXRvcnkyNTI4OTA0OTM="
+      category="General"
+      category-id="DIC_kwDODxLNfc4CV3Ab"
+      mapping="pathname"
+      strict="0"
+      reactions-enabled="1"
+      emit-metadata="0"
+      input-position="top"
+      theme="preferred_color_scheme"
+      lang="zh-CN"
+      loading="lazy"
       crossorigin="anonymous"
       async
     />
